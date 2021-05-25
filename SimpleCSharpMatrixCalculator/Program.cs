7943559815475ad073c9aee0aab35be0981e7e19
Dalalaler <dalalaler@gmail.com>
2021-05-25 21:04:01 +0300

    Add project files.

---
+++ SimpleCSharpMatrixCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCSharpMatrixCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
